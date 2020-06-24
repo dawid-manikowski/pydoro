c9a9a497ed3c3f6e6060688c49cbfca8b8a16d02
Dawid Manikowski <dawid.manikowski@makeitright.pl>
2020-06-24 23:09:36 +0200

    Add init and module with consts

---
+++ consts.py
@@ -0,0 +1,10 @@
+PAUSE_AMOUNT = 4
+WORK_TIME = 1 * 25
+PAUSE_TIME = 1 * 5
+BREAK_TIME = 1 * 10
+ICON_PATH = "./788502_1.jpg"
+NOTIFICATIONS = {
+    "pause": "Shortie time!",
+    "break": "It's break time!",
+    "work": "Keep working!"
+}
